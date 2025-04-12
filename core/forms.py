bd396759fbc4486b38d85992be7d6902543096e6
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-04-12 16:30:58 -0500

    chore: Create custom out of stock exception

--- core/forms.py
+++ core/forms.py
@@ -15,7 +15,21 @@ class RestaurantForm(forms.ModelForm):
         fields = ('name', 'restaurant_type')
 
 
+class ProductStockException(Exception):
+    pass
+
+
 class ProductOrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('product', 'number_of_items')
+
+    def save(self, commit=True):
+        order = super().save(commit=False)
+
+        if order.number_of_items > order.product.number_in_stock:
+            raise ProductStockException(
+                f'Not enough items in stock for the product {order.product.name}')
+        if commit:
+            order.save()
+        return order
